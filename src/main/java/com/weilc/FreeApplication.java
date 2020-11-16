4934bbfe166ab6c784a4197a1d292e2bf6f19b7a
weiliangchun <840833211@qq.com>
2020-11-16 14:43:42 +0800

    git init

---
+++ src/main/java/com/weilc/FreeApplication.java
@@ -0,0 +1,13 @@
+package com.weilc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weilc/FreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
